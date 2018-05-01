d53e18f70a6e9dbe9aca5f43fa46f884c34fcf17
Rene Rubio <rubio05@gmail.com>
2018-05-01 12:16:46 -0400

    Assessment Module 1

---
+++ converter.js
@@ -0,0 +1,39 @@
+
+
+const csv = require('csvtojson')
+const fs = require('fs')
+const path = require('path')
+
+
+const convertToJson = (cvsPathFile='customer-data.csv') => {
+        const convertData = (cvsFilePath, callback)=>{
+            let jsonData = []
+            csv()
+            .fromFile(cvsPathFile)
+            .on('json',(jsonObj)=>{
+                jsonData.push(jsonObj) 
+            })
+            .on('done',(error)=>{
+                console.log('Conversion process finished.')
+                callback(null, jsonData)
+            })
+            .on('error', (error)=>{
+                callback(error)
+            })
+        }
+
+        convertData(cvsPathFile, (error, data)=>{
+        if (error) {
+            console.log('Error: ' + error)
+        }
+        else
+        {
+            fs.writeFileSync(path.join(__dirname, 'data.json'),JSON.stringify(data))
+            console.log('New Json file saved.')
+        }
+        global.process.exit(0)
+        })
+}  
+
+convertToJson(process.argv[2])
+
